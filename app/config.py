204b0083ec2e0b2082983309d64bd5dd32d463bc
NeilHamilton <neil.hamilton@uk.ey.com>
2020-03-19 22:27:38 +0000

    added webform

--- app/config.py
+++ app/config.py
@@ -2,4 +2,5 @@ import os
 
 
 class Config(object):
+    # gets the SECRET_KEY from an environment variable or defaults to string
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
--- app/forms.py
+++ app/forms.py
@@ -5,6 +5,12 @@ from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
+    """When the browser sends the POST request as a result of pressing submit,
+    form.validate_on_submit() gathers all the data, runs the validators,
+    and  will return True, indicating that the data is valid.
+    If one field fails validation, then the function will return False,
+    causing the form to be rendered back to the user, like with GET."""
+
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
