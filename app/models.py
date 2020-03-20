a2c896076a6bd8d58b90ac341b8af1e3a1f77f31
NeilHamilton <neil.hamilton@uk.ey.com>
2020-03-20 15:08:18 +0000

    created user table db

---
+++ app/models.py
@@ -0,0 +1,15 @@
+from app import db
+
+
+class User(db.Model):
+    """Inherits db.Model from Flask-SQLAlchemy, where it is
+    a base class for all models. The following code defines
+    the database schema."""
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+
+    def __repr__(self):
+        """This tells python how to print object of this class"""
+        return '<User {}.'.format(self.username)
