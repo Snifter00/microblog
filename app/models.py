766e0e6e2185b049c7a2a7f50b1b400e062cb30d
NeilHamilton <neil.hamilton@uk.ey.com>
2020-03-20 15:28:50 +0000

    first db migration

--- app/models.py
+++ app/models.py
@@ -9,6 +9,7 @@ class User(db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    # __tablename__= string to overide default table name, 'user'
 
     def __repr__(self):
         """This tells python how to print object of this class"""
