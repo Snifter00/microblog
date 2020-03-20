70f38ff9ca7cb2b128bfda55d89af04cbcbdc81f
NeilHamilton <neil.hamilton@uk.ey.com>
2020-03-20 13:46:35 +0000

    db changes

--- app/config.py
+++ app/config.py
@@ -1,6 +1,10 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
     # gets the SECRET_KEY from an environment variable or defaults to string
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False  # this stops Flask from signalling DB changes
