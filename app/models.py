7744084e8a7ff94703cbc9992704922a0f6783e3
NeilHamilton <neil.hamilton@uk.ey.com>
2020-03-21 21:17:24 +0000

    msg

--- app/models.py
+++ app/models.py
@@ -9,7 +9,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
+    #   password_hash = db.Column(db.String(128))
     # __tablename__= string to overide default table name, 'user'
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
