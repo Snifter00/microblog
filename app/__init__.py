896d98ec6e28874acdf223aecbb6afd569666873
NeilHamilton <neil.hamilton@uk.ey.com>
2020-03-23 21:48:51 +0000

    log in work

--- app/__init__.py
+++ app/__init__.py
@@ -10,5 +10,6 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)  # this var represents the migration engine
 login = LoginManager(app)
+login.login_view = 'login'
 
 from app import routes, models
