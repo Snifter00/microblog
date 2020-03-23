8de26691c63571b62597c6f317ba1529246890d8
NeilHamilton <neil.hamilton@uk.ey.com>
2020-03-23 12:06:53 +0000

    update db

--- app/models.py
+++ app/models.py
@@ -6,11 +6,12 @@ class User(db.Model):
     """Inherits db.Model from Flask-SQLAlchemy, where it is
     a base class for all models. The following code defines
     the database schema."""
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
-    #   password_hash = db.Column(db.String(128))
-    # __tablename__= string to overide default table name, 'user'
+    password_hash = db.Column(db.String(128))
+    # __tablename__= string to overide default snake_case table name, e.g. 'user'
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __repr__(self):
