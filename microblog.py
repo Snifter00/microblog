af4d36bf1ea3d2addbb48e9efff1635883bef6df
NeilHamilton <neil.hamilton@uk.ey.com>
2020-03-23 13:49:09 +0000

    update microblog.py

--- microblog.py
+++ microblog.py
@@ -2,6 +2,8 @@ from app import app, db
 from app.models import User, Post
 
 
+# Creates a shell context annd adds the database instance and models to the session
+# run 'flask shell' to invoke this function.
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'post': Post}
+    return {'db': db, 'User': User, 'Post': Post}
