13d2c7f752b5cf39c3659645407acd031ccb9750
NeilHamilton <neil.hamilton@uk.ey.com>
2020-03-25 12:44:01 +0000

    more changes

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,7 @@
 ''' Log in form '''
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired, ValidationError, DataRequired, Email, EqualTo, Length
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo, Length
 from app.models import User
 
 
