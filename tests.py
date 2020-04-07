9c4210a4698c6ad3119d879a4aad9b398f148b3d
NeilHamilton <neil.hamilton@uk.ey.com>
2020-04-07 22:34:00 +0100

    changed avatar test condition http

--- tests.py
+++ tests.py
@@ -21,7 +21,7 @@ class UserModelCase(unittest.TestCase):
 
     def test_avatar(self):
         u = User(username='john', email='john@example.com')
-        self.assertEqual(u.avatar(128), ('https://www.gravatar.com/avatar/'
+        self.assertEqual(u.avatar(128), ('http://www.gravatar.com/avatar/'
                                          'd4c74594d841139328695756648b6bd6'
                                          '?d=identicon&s=128'))
 
