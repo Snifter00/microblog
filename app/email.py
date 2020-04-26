afdf2ce58a5fbd1f67ad0eb5b1f427e3f7847e34
NeilHamilton <neil.hamilton@uk.ey.com>
2020-04-26 23:18:05 +0100

    Resetting a User Password 2

--- app/email.py
+++ app/email.py
@@ -1,4 +1,5 @@
-from flask import Message, render_template
+from flask import render_template
+from flask_mail import Message
 from app import app, mail
 
 
@@ -15,6 +16,6 @@ def send_password_reset_email(user):
                sender=app.config['ADMINS'][0],
                recipients=[user.email],
                text_body=render_template('email/reset_password.txt',
-                                         user=user, token=token),
+                                          user=user, token=token),
                html_body=render_template('email/reset_password.html',
-                                         user=user, token=token))
+                                          user=user, token=token))
