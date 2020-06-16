d9872c74550d53c9ff896101c8400acfe61f8572
NeilHamilton <neil.hamilton@uk.ey.com>
2020-06-16 22:16:24 +0100

    introduction to flask babel

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,8 @@ from flask_mail import Mail
 from app.config import Config
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_babel import Babel
+from flask import request
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -19,6 +21,11 @@ login.login_view = 'login'
 mail = Mail(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+babel = Babel(app)
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
