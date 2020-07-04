c2dadcd3d6c982973e84191ba35d393ae7fc9c42
NeilHamilton <neil.hamilton@uk.ey.com>
2020-07-04 16:30:45 +0100

    Application factory pattern

--- microblog.py
+++ microblog.py
@@ -1,10 +1,13 @@
 # This element defines the Flask application instance
-from app import app, db, cli
+from app import create_app, db, cli
 from app.models import User, Post
 
+app = create_app()
+cli.register(app)
 
 # Creates a shell context annd adds the database instance and models to the session
 # run 'flask shell' to invoke this function.
+
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Post': Post}
